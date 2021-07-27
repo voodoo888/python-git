b2393e494da139ea5b91e058e897c7dad68bc437
Korolev Vladimir <developmest@gamil.com>
2021-07-27 17:42:50 +0300

    #4 po tz peredelal

--- #4.py
+++ #4.py
@@ -9,3 +9,14 @@ while i < a:
         number_one = int(number[i])
     i += 1
 print(f'Наибольшая цифра: {number_one}')
+
+# Вариант 2
+
+number_max = 0
+number = int(number)
+while number:
+    result = number % 10
+    if result > number_max:
+        number_max = result
+    number = number // 10
+print('Масимальная цифра (вариант 2): ', number_max)
